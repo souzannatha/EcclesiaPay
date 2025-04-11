b4d9cade63738859fe4d95fe2a7639420ca87a6e
souzannatha <souzanatha04@gmail.com>
2025-04-11 19:28:38 -0300

    fix: added changes in table

--- src/app/modules/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard.component.ts
@@ -1,24 +1,25 @@
 import { Component } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 
+type payment = 'Pix' | 'Dinheiro' | 'Sem pagamento';
 interface PeriodicElement {
   name: string;
   position: number;
-  weight: number;
-  symbol: string;
+  paid: string;
+  paymentMethod: payment;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
+  { position: 1, name: 'Hydrogen', paid: 'Sim', paymentMethod: 'Pix' },
+  { position: 2, name: 'Helium', paid: 'Sim', paymentMethod: 'Dinheiro' },
+  { position: 3, name: 'Lithium', paid: 'Sim', paymentMethod: 'Pix' },
+  { position: 4, name: 'Beryllium', paid: 'Sim', paymentMethod: 'Dinheiro' },
+  { position: 5, name: 'Boron', paid: 'Sim', paymentMethod: 'Pix' },
+  { position: 6, name: 'Carbon', paid: 'Sim', paymentMethod: 'Pix' },
+  {position: 7,name: 'Nitrogen',paid: 'Não',paymentMethod: 'Sem pagamento',},
+  { position: 8, name: 'Oxygen', paid: 'Não', paymentMethod: 'Sem pagamento' },
+  {position: 9,name: 'Fluorine',paid: 'Não',paymentMethod: 'Sem pagamento' },
+  { position: 10, name: 'Neon', paid: 'Não', paymentMethod: 'Sem pagamento' },
 ];
 @Component({
   selector: 'app-dashboard',
@@ -26,7 +27,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = ['position', 'name', 'paid', 'paymentMethod'];
   dataSource = new MatTableDataSource(ELEMENT_DATA);
 
   applyFilter(event: Event) {
