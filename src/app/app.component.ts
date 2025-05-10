e538d054652b144c71c0b66c337627b8b9d864df
souzannatha <souzanatha04@gmail.com>
2025-05-10 18:02:27 -0300

    feat: added toolbar logic in login screen.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  showToolbar = false;
   title = 'EcclesiaPay';
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.showToolbar = event.url !== '/';
+      }
+    });
+  }
 }
